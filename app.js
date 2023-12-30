119024e8588382fb6e7e59ba03efe046c4868b77
Kranti Kumar Shah <Krantikumar803@gmail.com>
2023-12-30 16:15:45 +0545

    setup cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { config } from 'dotenv';
 import { CustomError } from './middlewares/CustomError.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 config({
     path: './config/config.env'
 })
@@ -9,6 +10,13 @@ export const app = express();
 
 //to parse the cookie from req.cookie
 app.use(cookieParser());
+
+//cors
+app.use(cors({
+    credentials:true,
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE']
+}))
 
 //to parse req.body to json
 app.use(express.json());
