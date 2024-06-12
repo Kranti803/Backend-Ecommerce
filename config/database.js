6b3de83f4eeb7e804d3cf6c1d6269bcca633d7f7
Kranti Kumar Shah <Krantikumar803@gmail.com>
2024-06-12 17:14:11 +0545

    fixing db connection

--- config/database.js
+++ config/database.js
@@ -1,9 +1,8 @@
 import mongoose from "mongoose";
 mongoose.set('strictQuery', true);
 
-
 export const connectDB = () => {
-
-    mongoose.connect(process.env.MONGODB_URI).then(data => console.log(`DB is connected to : ${data.connection.host}`)).catch((error) => console.log(error))
-
+  mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
+    console.log(`Connected to with server :${data.connection.host}`);
+  })
 }
